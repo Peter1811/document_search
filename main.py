9a9ea3500e791052de8f77b27f098c7e693f0b27
Peter1811 <78861235+Peter1811@users.noreply.github.com>
2024-09-11 15:07:01 +0300

    reworked search endpoint, added requirements file

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import datetime
-import random
-
 from sqlalchemy import select
 
 from fastapi import Body, Depends, FastAPI, HTTPException
@@ -8,25 +5,31 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from database import session
-from models import Document, Rubric
-from utils import generate_rubric_name
+from models import Document
 
 app = FastAPI()
 
 def get_db():
-    with session() as sess:
-        yield sess
+    with session() as db:
+        yield db
 
 
 @app.post('/search/')
 def text_search(text: str = Body(...), db: Session = Depends(get_db)):
-    request = select(Document)
-    documents = list(db.execute(request).scalars())
-    results = {}
-    for document in documents:
-        results[document.id] = [document.created_data, document.text, [rubric.text for rubric in document.rubrics]]
-    sorted_results = dict(sorted(results.items(), key=lambda x: x[1][0]))
-    return {'res': list(sorted_results.items())}
+    try:
+        request = select(Document)
+        documents = list(db.execute(request).scalars())
+        if len(documents) == 0:
+            return JSONResponse(content={'detail': 'No items in table'})
+        
+        results = {}
+        for document in documents:
+            results[document.id] = [document.created_data, document.text, [rubric.text for rubric in document.rubrics]]
+        sorted_results = dict(sorted(results.items(), key=lambda x: x[1][0]))
+        return {'res': list(sorted_results.items())}
+    except Exception as exc:
+        return JSONResponse(content={'detail': str(exc)})
+
 
 @app.delete('/delete/{id:int}')
 def delete_document(id: int, db: Session = Depends(get_db)):
@@ -44,6 +47,6 @@ def delete_document(id: int, db: Session = Depends(get_db)):
         if exception.status_code == 404:
             return JSONResponse(status_code=404, content={'detail': 'Document was not found'})
 
-    except Exception as e:
+    except Exception as exc:
         db.rollback()
-        return JSONResponse(content={'detail': 'Internal error'})
+        return JSONResponse(content={'detail': 'Internal error: ' + str(exc)})
