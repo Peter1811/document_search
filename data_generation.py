25382118f9e24ea196f9053ad30b432aa8ba7693
Peter1811 <78861235+Peter1811@users.noreply.github.com>
2024-09-12 17:23:03 +0300

    changed data_generation.py file

--- data_generation.py
+++ data_generation.py
@@ -1,29 +1,28 @@
 # скрипт запускается перед запуском сервера, для заполнения БД
 
+import os
 import pandas as pd
+
+from elasticsearch import Elasticsearch
+from dotenv import load_dotenv
 
 from database import session
 from models import Document
 
-from elasticsearch import Elasticsearch
-
-client = Elasticsearch('http://localhost:9200')
+load_dotenv()
+es_host = os.getenv('ELASTICSEARCH_HOST')
+es_port = os.getenv('ELASTICSEARCH_PORT')
+client = Elasticsearch(f'http://{es_host}:{es_port}')
 
 with session() as db:
-
     posts = pd.read_csv('posts.csv')
-
     for item in posts.values:
         item_dict = dict(zip(['Text', 'Date', 'Rubrics'], item))
-
-        print(item_dict['Rubrics'])
         new_doc = Document(
             text=item_dict['Text'],
             created_date=item_dict['Date'],
             rubrics=eval(item_dict['Rubrics'])
         )
-
-        
 
         db.add(new_doc)
         db.commit()
